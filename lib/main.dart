33f044874f58e6e63f0d4ca9e064e51afca2a2f9
bertauchekurtis <kurtisbertauche@gmail.com>
2023-10-25 21:32:48 -0700

    added map dependency

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 4, 30, 66)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 4, 30, 66)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Ur Next Route'),
