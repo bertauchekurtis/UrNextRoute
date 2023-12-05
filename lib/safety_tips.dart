fdc27dba20a55439fb3696bee3592188a1bd2fce
Munakash-Michael <mmunakash@nevada.unr.edu>
2023-12-05 09:08:49 -0800

    Safety Tool Kit

--- lib/safety_tips.dart
+++ lib/safety_tips.dart
@@ -31,27 +31,27 @@ class SafetyTips extends StatelessWidget {
             title: Text("Be aware of your surroundings"),
           ),
           ListTile(
-            leading: Icon(Icons.pin_drop_outlined),
+//            leading: Icon(Icons.pin_drop_outlined),
             title: Text("Walk in the center of the sidewalk, away from building doorways, hedges, and parked cars"),
           ), 
           ListTile(
-            leading: Icon(Icons.pin_drop_outlined),
+//            leading: Icon(Icons.pin_drop_outlined),
             title: Text("Try to avoid walking alone at night"),
           ),
           ListTile(
-            leading: Icon(Icons.pin_drop_outlined),
+//            leading: Icon(Icons.pin_drop_outlined),
             title: Text("Avoid walking down dark, vacant, or deserted"),
           ),
           ListTile(
-            leading: Icon(Icons.pin_drop_outlined),
+//            leading: Icon(Icons.pin_drop_outlined),
             title: Text("Always look where you are going, do not get distracted with your phone"),
           ),
           ListTile(
-            leading: Icon(Icons.pin_drop_outlined),
+//            leading: Icon(Icons.pin_drop_outlined),
             title: Text("Tuck gold chains and other jewelry that might attract a criminal's attention inside your clothing"),
           ),
           ListTile(
-            leading: Icon(Icons.pin_drop_outlined),
+//            leading: Icon(Icons.pin_drop_outlined),
             title: Text("Tuck gold chains and other jewelry that might attract a criminal's attention inside your clothing"),
           ),      
         ],
