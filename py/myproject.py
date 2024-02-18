9769c9bf2e2969d907327922a74aa4b57cb2b7d6
bertauchekurtis <kurtisbertauche@gmail.com>
2024-02-18 12:13:34 -0800

    one final db init check

--- py/myproject.py
+++ py/myproject.py
@@ -7,7 +7,7 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1)
 @app.route("/")
 def home():
     result = db.get_user()
-    return f"<h1 style='color:blue'>hello worlds! {result[1]} {result[2]}</h1>"
+    return f"<h1 style='color:blue'>hello world - db result: {result[1]} {result[2]}</h1>"
 
 @app.route('/json')
 def json():
